5673b8383d4720aa4d7ca435654d20108c00a17b
andrew-merz <merz.school@gmail.com>
2022-06-04 17:03:48 -0400

    user model added

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "ex. abc123@gmail.com"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please create a password"],
+  },
+  profilePic: {
+    type: String,
+    default:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdpRvftRBgfCbvzOHB0bANVih3QvZD-xZ4flbABUFGDctmaY87ajkJD5RhdvVcyZvkS7U&usqp=CAU",
+  },
+  status: {
+    type: mongoose.Types.ObjectId,
+    ref: "Status",
+  },
+  roommates: {
+    type: String,
+    required: [true, "Add Roommates"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
